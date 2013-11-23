42f15d3f3929199dd81309ae1eb80ff9fc370064
Ivan Calderon <iwano@hotmail.com>
2013-11-22 23:52:09 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Perronapp::Application.config.secret_key_base = 'f8e128ade131ebb6cb8aced0a0770f22bcd69a1fa184564e76c0399fad2f9e389ff815e2a8a69aca48eed087601811311b22e26aec4b33af4ad663ed60ebf48c'
