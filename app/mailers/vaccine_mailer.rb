64a46565c931b4cd2043195545a2ad2f650a914b
Jaime Victoria <jaime.victoria@crowdint.com>
2013-11-24 16:35:03 -0600

    Change email receiver

--- app/mailers/vaccine_mailer.rb
+++ app/mailers/vaccine_mailer.rb
@@ -2,6 +2,6 @@ class VaccineMailer < ActionMailer::Base
   default from: "noreply@perronapp.com"
 
   def vaccine_email
-    mail(to: 'mail@eduardobautista.com', subject: 'Vacunalerta')
+    mail(to: 'diaz333@gmail.com', subject: 'Vacunalerta')
   end
 end
